9c7aae44e983200cb49358dc681ee2533846eb83
Christian <christian@fivesqrd.com>
2019-02-19 23:14:10 +0200

    New update actions: add and delete

--- src/Item.php
+++ src/Item.php
@@ -50,6 +50,46 @@ class Item
         return $this;
     }
 
+    public function add($key, $value)
+    {
+        $this->_diff[] = [
+            'attribute' => $key,
+            'value'     => $value,
+            'action'    => 'add'
+        ];
+
+        if (!is_array($this->_attributes[$key])) {
+            $this->_attributes[$key] = [];
+        }
+
+        array_push($this->_attributes[$key], $value);
+
+        return $this;
+    }
+
+    public function delete($key, $value)
+    {
+        if (!is_array($this->attribute($key))) {
+            return false;
+        }
+        
+        if (!in_array($this->attribute($key), $value)) {
+            return false;
+        }
+
+        $this->_diff[] = [
+            'attribute' => $key,
+            'value'     => $value,
+            'action'    => 'delete'
+        ];
+
+        $index = array_search($value, $this->attribute($key));
+
+        unset($this->_attributes[$key][$index]);
+
+        return $this;
+    }
+
     public function remove($key)
     {
         if (!array_key_exists($key, $this->_attributes)) {
